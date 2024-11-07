7a44697b0d1b52218d726ad8c26b43e7cf47bb11
Parth Bhardwaj <69952086+PixelVibe@users.noreply.github.com>
2024-11-07 09:17:28 +0530

    Create p2.js

---
+++ Day 7/p2.js
@@ -0,0 +1,20 @@
+async function orderfood(name,time){
+    return new Promise((resolve,reject)=>{
+        if(name="Burger")reject("Burger not available");
+        setTimeout(()=>{
+            resolve("Order for "+name+" Prepared");
+        },time);
+    });
+}
+
+async function restraurant(){
+    console.log("Place Order");
+    let pizza=await orderfood("Pizza",2000);
+    console.log(pizza);
+    let burger=await orderfood("Burger",3000);
+    console.log(burger);
+    let noodles=orderfood("Noodels",4000)
+    console.log(noodles)
+    console.log("All order completed")
+}
+restraurant();
