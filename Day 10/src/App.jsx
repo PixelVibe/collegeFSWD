eb8e508054c8ece8b908f4c26f5ee45e1187d313
Parth Bhardwaj <69952086+PixelVibe@users.noreply.github.com>
2024-12-06 14:05:55 +0530

    Add files via upload

---
+++ Day 10/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CounterApp from "./components/CounterApp.jsx";
+
+const App = () => {
+  return  (
+      <div><CounterApp /></div>
+);
+      };
+
+export default App;
